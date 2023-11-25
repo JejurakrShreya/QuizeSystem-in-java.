31c202e742d9ee146768a2891b0d0e936e18dbb2
ShreyaJejurkar <150332991+JejurakrShreya@users.noreply.github.com>
2023-11-25 13:30:19 +0530

    Add files via upload

---
+++ JOptionpane.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+class JOptionpane {
+
+    static int showConfirmDialog(Object object, String do_you__really_want_to_submit, String select, int YES_NO_OPTION) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
